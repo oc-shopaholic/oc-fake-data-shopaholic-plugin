83d575f017f2fe377d92e1b9b98a5c91dcff273e
Tomaž Žitnik <tomaz.zitnik@spletna-postaj.com>
2020-10-01 12:53:36 +0200

    Slovenian language added

---
+++ lang/sl/lang.php
@@ -0,0 +1,32 @@
+<?php return [
+    'plugin'    => [
+        'name'        => 'Fake data for Shopaholic',
+        'description' => '',
+    ],
+    'field'     => [
+        'clothes'  => 'Obleke',
+        'sneakers' => 'Superge',
+        'fruits'   => 'Sadje',
+        'content'  => 'Vsebina',
+        'repeat'   => 'Ponavljanje',
+        'theme'    => 'Tema',
+    ],
+    'component' => [],
+    'tab'       => [],
+    'widget'    => [
+        'fake_data' => 'Ponarejanje podatkov za Shopaholic',
+    ],
+    'message'   => [
+        'generate_fake_data'                                => 'Ustvari ponarejene podatke',
+        'generate_fake_data_for_theme'                      => 'Ustvari ponarejene podatke za temo',
+        'select_content_type'                               => 'Izberi tip vsebine',
+        'how_many_times_to_repeat_the_creating_of_products' => 'Kolikokrat naj se ponovi ustvarjanje produktov? Vnesite vrednost od 1 do 1000',
+        'select_theme'                                      => 'Izberi temo',
+    ],
+    'button'    => [
+        'generate' => 'Ustvari',
+    ],
+    'theme'     => [
+        'sneakers' => 'Superge',
+    ],
+];
